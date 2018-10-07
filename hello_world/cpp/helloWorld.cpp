f514c5531a31e3c56e18a7230ea772a2bd533cc7
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-07 14:56:26 -0400

    cpp example-hello world

---
+++ hello_world/cpp/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    cout<<"Hello World without c style newline(escape character)"<<endl;
+    cout<<"Hello World with new line\n";
+    return 0;
+}
