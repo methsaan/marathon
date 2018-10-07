c5bff9f9be78b399f44e56eed016eba77501ce2c
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-07 14:38:50 -0400

    update

---
+++ hello_world/c/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+    printf("Hello World without new line");
+    printf("Hello World with new line\n");
+    printf("Hello World with tab\t there\n");
+    puts("Hello world with puts");
+    return 0;
+}
---
+++ hello_world/java/HelloWorld.java
@@ -0,0 +1,12 @@
+// this is a single line comment
+/* this is a 
+  multi-line comment */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" Hello World using println");
+        System.out.print(" Hello World using print without newline");
+        System.out.print(" Hello World using print with new line\n");
+        System.out.printf(" Hello World using printf without newline");
+        System.out.printf(" Hello World using printf with newline \n");
+    }
+}
