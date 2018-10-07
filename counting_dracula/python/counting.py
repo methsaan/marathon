a5644c1dcbc1181d8b0598d9669b5a5e19f189ae
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-07 18:16:23 -0400

    update

---
+++ counting_dracula/python/counting.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+print("Using for loop")
+cnt=10
+for x in range(cnt):
+    print(cnt-x)
+
+print("Using while loop")
+cnt2 = 10
+
+while cnt2 > 0:
+    print(cnt2)
+    cnt2 = cnt2 - 1
