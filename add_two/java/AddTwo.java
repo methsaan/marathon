a661f493ae46f9ae2ea98cb20737fd1481959423
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-07 18:38:17 -0400

    add two

---
+++ add_two/java/AddTwo.java
@@ -0,0 +1,7 @@
+public class AddTwo{
+	public static void main(String[] args){
+		int x = 10;
+		int y = 20;
+		System.out.println(x+y);
+	}
+}
---
+++ add_two/python/addTwo.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+
+x = 10
+y = 20
+print(x+y)
