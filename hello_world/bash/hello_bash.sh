56b14011ebcab373e947bac6565fb52843c3619e
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-07 14:38:50 -0400

    update

---
+++ hello_world/bash/hello_bash.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+echo "Hello World with double quotes"
+echo Hello World without any quotes
+echo 'Hellow World with single quotes'
+printf 'Hello World using the printf command\n'
---
+++ hello_world/python/helloWorld.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+print("Hello world with newline")
+print('Hello world in single quotes')
+print("Hello world without newline ", end="", flush=True)
+print('Hello world in sigle quotes without newline ', end='', flush=True)
+
+print('Hello world in single quotes and escape with tab\tand that was tab!')
